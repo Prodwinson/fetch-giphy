2e78bdfb9e54ff4cec050ec19f3747130672539b
Prodwinson <Sywinson3@gmail.com>
2023-05-09 21:17:31 -0400

    commit

--- main.js
+++ main.js
@@ -8,12 +8,29 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=DGJXoVny0Ruubz0rNEKG31tnI
     images.src = response.data.images.original.url
 })
 
+// search button to change the gif
+const searchBtn = document.querySelector('#button')
+
+// click event for the search button
+searchBtn.addEventListener('click', () => {
+    searchGiphy()
+})
+
+// the function behind changing the gif 
+async function searchGiphy() {
+    let searchInput = document.querySelector('#search').value
+    if (searchInput === "") {
+        searchInput = "random"
+    }
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=DGJXoVny0Ruubz0rNEKG31tnIQ8yz4DC&s=' + searchInput, {mode: 'cors'})
+    const searchInputData = await response.json()
+    images.src = searchInputData.data.images.original.url
+}
+
 const body = document.querySelector('body')
 const btn = document.createElement('button')
 btn.addEventListener('click', () => {
     location.reload()
 })
-btn.textContent = "Change Image"
-
+btn.textContent = "Change Cat Gif"
 body.appendChild(btn)
-
